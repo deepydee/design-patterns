4685e6507db91e35aeea41eeea002dce110be218
Vitaliy Semenov <synthex@bk.ru>
2023-12-19 10:11:07 +0600

    factory method: social network connectors

---
+++ app-modules/factory-method/src/RealWorld/Connectors/LinkedInConnector.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\FactoryMethod\RealWorld\Connectors;
+
+use Modules\FactoryMethod\RealWorld\Contracts\SocialNetworkConnector;
+
+/**
+ * А этот Конкретный Продукт реализует API LinkedIn.
+ */
+final class LinkedInConnector implements SocialNetworkConnector
+{
+    public function __construct(private string $email, private string $password)
+    {
+    }
+
+    public function logIn(): void
+    {
+        echo "Send HTTP API request to log in user $this->email with " .
+            "password $this->password\n";
+    }
+
+    public function logOut(): void
+    {
+        echo "Send HTTP API request to log out user $this->email\n";
+    }
+
+    public function createPost($content): void
+    {
+        echo "Send HTTP API requests to create a post in LinkedIn timeline.\n";
+    }
+}
