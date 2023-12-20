d4fb0a289eafde579d593a7fad607018bc723103
Vitaliy Semenov <synthex@bk.ru>
2023-12-20 09:54:29 +0600

    abstract-factory: scaffoldings

---
+++ app-modules/abstract-factory/routes/abstract-factory-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\AbstractFactory\Http\Controllers\AbstractFactoryController;
+
+// Route::get('/abstract-factories', [AbstractFactoryController::class, 'index'])->name('abstract-factories.index');
+// Route::get('/abstract-factories/create', [AbstractFactoryController::class, 'create'])->name('abstract-factories.create');
+// Route::post('/abstract-factories', [AbstractFactoryController::class, 'store'])->name('abstract-factories.store');
+// Route::get('/abstract-factories/{abstract-factory}', [AbstractFactoryController::class, 'show'])->name('abstract-factories.show');
+// Route::get('/abstract-factories/{abstract-factory}/edit', [AbstractFactoryController::class, 'edit'])->name('abstract-factories.edit');
+// Route::put('/abstract-factories/{abstract-factory}', [AbstractFactoryController::class, 'update'])->name('abstract-factories.update');
+// Route::delete('/abstract-factories/{abstract-factory}', [AbstractFactoryController::class, 'destroy'])->name('abstract-factories.destroy');
