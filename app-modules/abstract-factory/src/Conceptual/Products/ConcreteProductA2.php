d732a886c9bfd5ab9c059accdd4cd29c245f3a2a
Vitaliy Semenov <synthex@bk.ru>
2023-12-21 09:49:38 +0600

    abstract-factory: conceptual

---
+++ app-modules/abstract-factory/src/Conceptual/Products/ConcreteProductA2.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\AbstractFactory\Conceptual\Products;
+
+use Modules\AbstractFactory\Conceptual\Contracts\AbstractProductA;
+
+/**
+ * Конкретные продукты создаются соответствующими Конкретными Фабриками.
+ */
+final class ConcreteProductA2 implements AbstractProductA
+{
+    public function usefulFunctionA(): string
+    {
+        return "The result of the product A2.";
+    }
+}
