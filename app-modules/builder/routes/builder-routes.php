5bd44231f2529826e6f0f4cf2e84fdf3f2d7755f
Vitaliy Semenov <synthex@bk.ru>
2023-12-26 09:34:26 +0600

    chore: builder module scaffoldings

---
+++ app-modules/builder/routes/builder-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Builder\Http\Controllers\BuilderController;
+
+// Route::get('/builders', [BuilderController::class, 'index'])->name('builders.index');
+// Route::get('/builders/create', [BuilderController::class, 'create'])->name('builders.create');
+// Route::post('/builders', [BuilderController::class, 'store'])->name('builders.store');
+// Route::get('/builders/{builder}', [BuilderController::class, 'show'])->name('builders.show');
+// Route::get('/builders/{builder}/edit', [BuilderController::class, 'edit'])->name('builders.edit');
+// Route::put('/builders/{builder}', [BuilderController::class, 'update'])->name('builders.update');
+// Route::delete('/builders/{builder}', [BuilderController::class, 'destroy'])->name('builders.destroy');
