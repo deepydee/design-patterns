9a4f8ed4b69d02ffb9c8f1984bacc2b234e0e60c
Vitaliy Semenov <synthex@bk.ru>
2023-12-27 10:45:32 +0600

    feat: singleton scaffoldings

---
+++ app-modules/singleton/routes/singleton-routes.php
@@ -0,0 +1,5 @@
+<?php
+
+// use Modules\Singleton\Http\Controllers\SingletonController;
+
+// Route::get('/singletons', [SingletonController::class, 'index'])->name('singletons.index');
---
+++ app-modules/singleton/src/Providers/SingletonServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Singleton\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SingletonServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
