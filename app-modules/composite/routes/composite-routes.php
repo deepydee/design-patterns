d1d7295e7b71debced85773547f4ed82c8dedb69
Vitaliy Semenov <synthex@bk.ru>
2024-01-07 11:48:18 +0600

    chore: composite scaffoldings

---
+++ app-modules/composite/routes/composite-routes.php
@@ -0,0 +1,5 @@
+<?php
+
+// use Modules\Composite\Http\Controllers\CompositeController;
+
+// Route::get('/composites', [CompositeController::class, 'index'])->name('composites.index');
---
+++ app-modules/composite/src/Providers/CompositeServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Composite\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CompositeServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
