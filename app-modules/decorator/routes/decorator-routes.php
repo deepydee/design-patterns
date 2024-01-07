799edf67319f149f60ed1f0fe711886ae6c920a5
Vitaliy Semenov <synthex@bk.ru>
2024-01-07 14:48:54 +0600

    chore: decorator scaffoldings

---
+++ app-modules/decorator/routes/decorator-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Decorator\Http\Controllers\DecoratorController;
+
+// Route::get('/decorators', [DecoratorController::class, 'index'])->name('decorators.index');
+// Route::get('/decorators/create', [DecoratorController::class, 'create'])->name('decorators.create');
+// Route::post('/decorators', [DecoratorController::class, 'store'])->name('decorators.store');
+// Route::get('/decorators/{decorator}', [DecoratorController::class, 'show'])->name('decorators.show');
+// Route::get('/decorators/{decorator}/edit', [DecoratorController::class, 'edit'])->name('decorators.edit');
+// Route::put('/decorators/{decorator}', [DecoratorController::class, 'update'])->name('decorators.update');
+// Route::delete('/decorators/{decorator}', [DecoratorController::class, 'destroy'])->name('decorators.destroy');
