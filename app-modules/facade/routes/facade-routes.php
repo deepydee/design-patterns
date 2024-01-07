154841d95cbdfdfb82885a7200e90e3454d7be79
Vitaliy Semenov <synthex@bk.ru>
2024-01-07 23:37:53 +0600

    chore: facade initial scaffoldings

---
+++ app-modules/facade/routes/facade-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Facade\Http\Controllers\FacadeController;
+
+// Route::get('/facades', [FacadeController::class, 'index'])->name('facades.index');
+// Route::get('/facades/create', [FacadeController::class, 'create'])->name('facades.create');
+// Route::post('/facades', [FacadeController::class, 'store'])->name('facades.store');
+// Route::get('/facades/{facade}', [FacadeController::class, 'show'])->name('facades.show');
+// Route::get('/facades/{facade}/edit', [FacadeController::class, 'edit'])->name('facades.edit');
+// Route::put('/facades/{facade}', [FacadeController::class, 'update'])->name('facades.update');
+// Route::delete('/facades/{facade}', [FacadeController::class, 'destroy'])->name('facades.destroy');
