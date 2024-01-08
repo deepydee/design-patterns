f2904ebc19929e9f96474fa5b768b37e6f6b9a46
Vitaliy Semenov <synthex@bk.ru>
2024-01-08 19:26:03 +0600

    feat: implement conceptual proxy

--- app-modules/proxy/routes/proxy-routes.php
+++ app-modules/proxy/routes/proxy-routes.php
@@ -1,11 +1,5 @@
 <?php
 
-// use Modules\Proxy\Http\Controllers\ProxyController;
+use Modules\Proxy\Http\Controllers\ProxyConceptualController;
 
-// Route::get('/proxies', [ProxyController::class, 'index'])->name('proxies.index');
-// Route::get('/proxies/create', [ProxyController::class, 'create'])->name('proxies.create');
-// Route::post('/proxies', [ProxyController::class, 'store'])->name('proxies.store');
-// Route::get('/proxies/{proxy}', [ProxyController::class, 'show'])->name('proxies.show');
-// Route::get('/proxies/{proxy}/edit', [ProxyController::class, 'edit'])->name('proxies.edit');
-// Route::put('/proxies/{proxy}', [ProxyController::class, 'update'])->name('proxies.update');
-// Route::delete('/proxies/{proxy}', [ProxyController::class, 'destroy'])->name('proxies.destroy');
+Route::get('/proxy/conceptual', ProxyConceptualController::class);
---
+++ app-modules/proxy/src/Conceptual/RealSubject.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Proxy\Conceptual;
+
+use Modules\Proxy\Conceptual\Contracts\Subject;
+
+/**
+ * Реальный Субъект содержит некоторую базовую бизнес-логику. Как правило,
+ * Реальные Субъекты способны выполнять некоторую полезную работу, которая к
+ * тому же может быть очень медленной или точной – например, коррекция входных
+ * данных. Заместитель может решить эти задачи без каких-либо изменений в коде
+ * Реального Субъекта.
+ */
+class RealSubject implements Subject
+{
+    public function request(): void
+    {
+        echo "RealSubject: Handling request.<br>";
+    }
+}
