fa46d792b8c03f51eb9138559934ccbc4e126b79
Vitaliy Semenov <synthex@bk.ru>
2024-01-08 17:25:12 +0600

    chore: init flyweight

---
+++ app-modules/flyweight/routes/flyweight-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Flyweight\Http\Controllers\FlyweightController;
+
+// Route::get('/flyweights', [FlyweightController::class, 'index'])->name('flyweights.index');
+// Route::get('/flyweights/create', [FlyweightController::class, 'create'])->name('flyweights.create');
+// Route::post('/flyweights', [FlyweightController::class, 'store'])->name('flyweights.store');
+// Route::get('/flyweights/{flyweight}', [FlyweightController::class, 'show'])->name('flyweights.show');
+// Route::get('/flyweights/{flyweight}/edit', [FlyweightController::class, 'edit'])->name('flyweights.edit');
+// Route::put('/flyweights/{flyweight}', [FlyweightController::class, 'update'])->name('flyweights.update');
+// Route::delete('/flyweights/{flyweight}', [FlyweightController::class, 'destroy'])->name('flyweights.destroy');
