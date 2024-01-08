d94a2a2123c624832f4d81b34589cfb2e0f4ffc3
Vitaliy Semenov <synthex@bk.ru>
2024-01-08 19:07:55 +0600

    chore: init proxy

---
+++ app-modules/proxy/routes/proxy-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Proxy\Http\Controllers\ProxyController;
+
+// Route::get('/proxies', [ProxyController::class, 'index'])->name('proxies.index');
+// Route::get('/proxies/create', [ProxyController::class, 'create'])->name('proxies.create');
+// Route::post('/proxies', [ProxyController::class, 'store'])->name('proxies.store');
+// Route::get('/proxies/{proxy}', [ProxyController::class, 'show'])->name('proxies.show');
+// Route::get('/proxies/{proxy}/edit', [ProxyController::class, 'edit'])->name('proxies.edit');
+// Route::put('/proxies/{proxy}', [ProxyController::class, 'update'])->name('proxies.update');
+// Route::delete('/proxies/{proxy}', [ProxyController::class, 'destroy'])->name('proxies.destroy');
