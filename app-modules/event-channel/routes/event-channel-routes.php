7fb61cb8b3151f7a423bd42ee346c612746b52d2
Vitaliy Semenov <synthex@bk.ru>
2024-01-09 16:08:01 +0600

    chore: init event channel

---
+++ app-modules/event-channel/routes/event-channel-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\EventChannel\Http\Controllers\EventChannelController;
+
+// Route::get('/event-channels', [EventChannelController::class, 'index'])->name('event-channels.index');
+// Route::get('/event-channels/create', [EventChannelController::class, 'create'])->name('event-channels.create');
+// Route::post('/event-channels', [EventChannelController::class, 'store'])->name('event-channels.store');
+// Route::get('/event-channels/{event-channel}', [EventChannelController::class, 'show'])->name('event-channels.show');
+// Route::get('/event-channels/{event-channel}/edit', [EventChannelController::class, 'edit'])->name('event-channels.edit');
+// Route::put('/event-channels/{event-channel}', [EventChannelController::class, 'update'])->name('event-channels.update');
+// Route::delete('/event-channels/{event-channel}', [EventChannelController::class, 'destroy'])->name('event-channels.destroy');
---
+++ app-modules/event-channel/tests/EventChannelServiceProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\EventChannel\Tests;
+
+class EventChannelServiceProviderTest extends TestCase
+{
+	// TODO
+}
