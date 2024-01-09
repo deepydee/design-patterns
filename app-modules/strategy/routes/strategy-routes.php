9390baa8950ed97a629d1097c890d6746f1b3d7f
Vitaliy Semenov <synthex@bk.ru>
2024-01-09 15:19:28 +0600

    feat: implement real world strategy

--- app-modules/strategy/routes/strategy-routes.php
+++ app-modules/strategy/routes/strategy-routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use Modules\Strategy\Http\Controllers\StrategyConceptualController;
+use Modules\Strategy\Http\Controllers\StrategyRealWorldController;
 
 Route::get('/strategy/conceptual', StrategyConceptualController::class);
+Route::get('/strategy/real-world', StrategyRealWorldController::class);
 
