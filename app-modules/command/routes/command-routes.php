14907a75e3bfa0b39bb32168faf2f4b5e89c16d3
Vitaliy Semenov <synthex@bk.ru>
2024-01-15 16:28:33 +0600

    chore: init command pattern

---
+++ app-modules/command/routes/command-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Command\Http\Controllers\CommandController;
+
+// Route::get('/commands', [CommandController::class, 'index'])->name('commands.index');
+// Route::get('/commands/create', [CommandController::class, 'create'])->name('commands.create');
+// Route::post('/commands', [CommandController::class, 'store'])->name('commands.store');
+// Route::get('/commands/{command}', [CommandController::class, 'show'])->name('commands.show');
+// Route::get('/commands/{command}/edit', [CommandController::class, 'edit'])->name('commands.edit');
+// Route::put('/commands/{command}', [CommandController::class, 'update'])->name('commands.update');
+// Route::delete('/commands/{command}', [CommandController::class, 'destroy'])->name('commands.destroy');
---
+++ app-modules/command/src/Providers/CommandServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Command\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
