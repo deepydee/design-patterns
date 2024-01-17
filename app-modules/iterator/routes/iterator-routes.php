fd9868c733eda6fadae6080bf709e5f4e20a0343
Vitaliy Semenov <synthex@bk.ru>
2024-01-17 17:52:25 +0600

    feat: real world iterators

--- app-modules/iterator/routes/iterator-routes.php
+++ app-modules/iterator/routes/iterator-routes.php
@@ -1,5 +1,7 @@
 <?php
 
 use Modules\Iterator\Http\Controllers\IteratorConceptualController;
+use Modules\Iterator\Http\Controllers\IteratorRealWorldController;
 
 Route::get('/iterator/conceptual', IteratorConceptualController::class);
+Route::get('/iterator/real-world', IteratorRealWorldController::class);
---
+++ app-modules/iterator/src/RealWorld/CSVIterator.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Iterator\RealWorld;
+
+/**
+ * Итератор CSV-файлов.
+ *
+ * @author Josh Lockhart
+ */
+class CSVIterator implements \Iterator
+{
+    private const ROW_SIZE = 4096;
+
+    /**
+     * Указатель на CSV-файл.
+     *
+     * @var resource
+     */
+    protected $filePointer = null;
+
+    /**
+     * Текущий элемент, который возвращается на каждой итерации.
+     */
+    protected array $currentElement = [];
+
+    /**
+     * Счётчик строк.
+     */
+    protected int $rowCounter = 1;
+
+    /**
+     * Разделитель для CSV-файла.
+     */
+    protected ?string $delimiter = null;
+
+    /**
+     * Конструктор пытается открыть CSV-файл. Он выдаёт исключение при ошибке.
+     *
+     * @param string $file CSV-файл.
+     * @param string $delimiter Разделитель.
+     *
+     * @throws \Exception
+     */
+    public function __construct(string $file, string $delimiter = ',')
+    {
+        try {
+            $this->filePointer = fopen($file, 'rb');
+            $this->delimiter = $delimiter;
+        } catch (\Exception $e) {
+            throw new \Exception('The file "'.$file.'" cannot be read.');
+        }
+    }
+
+    /**
+     * Этот метод сбрасывает указатель файла.
+     */
+    public function rewind(): void
+    {
+        $this->rowCounter = 1;
+        rewind($this->filePointer);
+    }
+
+    /**
+     * Этот метод возвращает текущую CSV-строку в виде двумерного массива.
+     *
+     * @return array Текущая CSV-строка в виде двумерного массива.
+     */
+    public function current(): array
+    {
+        return $this->currentElement;
+    }
+
+    /**
+     * Этот метод возвращает номер текущей строки.
+     *
+     * @return int Номер текущей строки.
+     */
+    public function key(): int
+    {
+        return $this->rowCounter;
+    }
+
+    /**
+     * Этот метод проверяет, достигнут ли конец файла.
+     *
+     * @return bool Возвращает true при достижении EOF, в ином случае false.
+     */
+    public function next(): void
+    {
+        $this->currentElement = fgetcsv($this->filePointer, self::ROW_SIZE, $this->delimiter);
+        $this->rowCounter++;
+    }
+
+    /**
+     * Этот метод проверяет, является ли следующая строка допустимой.
+     *
+     * @return bool Если следующая строка является допустимой.
+     */
+    public function valid(): bool
+    {
+        if (is_resource($this->filePointer)) {
+            if (feof($this->filePointer)) {
+                fclose($this->filePointer);
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+}
