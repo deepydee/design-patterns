49aa66afeff111361617520cceea7ff2ef5fb7c8
Vitaliy Semenov <synthex@bk.ru>
2024-01-17 18:48:58 +0600

    feat: impl conceptual observer

--- app-modules/observer/routes/observer-routes.php
+++ app-modules/observer/routes/observer-routes.php
@@ -1,11 +1,6 @@
 <?php
 
-// use Modules\Observer\Http\Controllers\ObserverController;
+use Modules\Observer\Http\Controllers\ObserverConceptualController;
 
-// Route::get('/observers', [ObserverController::class, 'index'])->name('observers.index');
-// Route::get('/observers/create', [ObserverController::class, 'create'])->name('observers.create');
-// Route::post('/observers', [ObserverController::class, 'store'])->name('observers.store');
-// Route::get('/observers/{observer}', [ObserverController::class, 'show'])->name('observers.show');
-// Route::get('/observers/{observer}/edit', [ObserverController::class, 'edit'])->name('observers.edit');
-// Route::put('/observers/{observer}', [ObserverController::class, 'update'])->name('observers.update');
-// Route::delete('/observers/{observer}', [ObserverController::class, 'destroy'])->name('observers.destroy');
+Route::get('/observer/conceptual', ObserverConceptualController::class);
+
