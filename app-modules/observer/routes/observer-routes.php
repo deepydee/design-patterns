5dd6d0893b57cb5498529f9d312d45c274f3b37a
Vitaliy Semenov <synthex@bk.ru>
2024-01-17 18:04:49 +0600

    chore: init observer

---
+++ app-modules/observer/routes/observer-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Observer\Http\Controllers\ObserverController;
+
+// Route::get('/observers', [ObserverController::class, 'index'])->name('observers.index');
+// Route::get('/observers/create', [ObserverController::class, 'create'])->name('observers.create');
+// Route::post('/observers', [ObserverController::class, 'store'])->name('observers.store');
+// Route::get('/observers/{observer}', [ObserverController::class, 'show'])->name('observers.show');
+// Route::get('/observers/{observer}/edit', [ObserverController::class, 'edit'])->name('observers.edit');
+// Route::put('/observers/{observer}', [ObserverController::class, 'update'])->name('observers.update');
+// Route::delete('/observers/{observer}', [ObserverController::class, 'destroy'])->name('observers.destroy');
