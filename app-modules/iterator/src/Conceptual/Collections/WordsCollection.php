419f7696ed8e64c5c1cb7ba3e94df436b181ce2f
Vitaliy Semenov <synthex@bk.ru>
2024-01-17 16:59:20 +0600

    feat: iterator conceptual

---
+++ app-modules/iterator/src/Conceptual/Collections/WordsCollection.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Iterator\Conceptual\Collections;
+use Modules\Iterator\Conceptual\Iterators\AlphabeticalOrderIterator;
+
+/**
+ * Конкретные Коллекции предоставляют один или несколько методов для получения
+ * новых экземпляров итератора, совместимых с классом коллекции.
+ */
+class WordsCollection implements \IteratorAggregate
+{
+    private array $items = [];
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function addItem(mixed $item): void
+    {
+        $this->items[] = $item;
+    }
+
+    public function getIterator(): \Iterator
+    {
+        return new AlphabeticalOrderIterator(collection: $this);
+    }
+
+    public function getReverseIterator(): \Iterator
+    {
+        return new AlphabeticalOrderIterator(collection: $this, reverse: true);
+    }
+}
