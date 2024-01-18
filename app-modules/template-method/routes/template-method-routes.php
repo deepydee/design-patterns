18002e32e5fd17f18babc9db7591bab776926d25
Vitaliy Semenov <synthex@bk.ru>
2024-01-18 13:53:11 +0600

    feat: conceptual template method impl

--- app-modules/template-method/routes/template-method-routes.php
+++ app-modules/template-method/routes/template-method-routes.php
@@ -1,11 +1,6 @@
 <?php
 
-// use Modules\TemplateMethod\Http\Controllers\TemplateMethodController;
+use Modules\TemplateMethod\Http\Controllers\TemplateMethodConceptualController;
 
-// Route::get('/template-methods', [TemplateMethodController::class, 'index'])->name('template-methods.index');
-// Route::get('/template-methods/create', [TemplateMethodController::class, 'create'])->name('template-methods.create');
-// Route::post('/template-methods', [TemplateMethodController::class, 'store'])->name('template-methods.store');
-// Route::get('/template-methods/{template-method}', [TemplateMethodController::class, 'show'])->name('template-methods.show');
-// Route::get('/template-methods/{template-method}/edit', [TemplateMethodController::class, 'edit'])->name('template-methods.edit');
-// Route::put('/template-methods/{template-method}', [TemplateMethodController::class, 'update'])->name('template-methods.update');
-// Route::delete('/template-methods/{template-method}', [TemplateMethodController::class, 'destroy'])->name('template-methods.destroy');
+Route::get('/template-method/conceptual', TemplateMethodConceptualController::class);
+
---
+++ app-modules/template-method/src/Http/Controllers/TemplateMethodConceptualController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\TemplateMethod\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Modules\TemplateMethod\Conceptual\AbstractClass;
+use Modules\TemplateMethod\Conceptual\ConcreteClass1;
+use Modules\TemplateMethod\Conceptual\ConcreteClass2;
+
+class TemplateMethodConceptualController extends Controller
+{
+    public function __invoke()
+    {
+        echo "Same client code can work with different subclasses:<br>";
+        $this->clientCode(new ConcreteClass1());
+        echo "<br>";
+
+        echo "Same client code can work with different subclasses:<br>";
+        $this->clientCode(new ConcreteClass2());
+    }
+
+    /**
+     * Клиентский код вызывает шаблонный метод для выполнения алгоритма. Клиентский
+     * код не должен знать конкретный класс объекта, с которым работает, при
+     * условии, что он работает с объектами через интерфейс их базового класса.
+     */
+    private function clientCode(AbstractClass $class)
+    {
+        // ...
+        $class->templateMethod();
+        // ...
+    }
+}
