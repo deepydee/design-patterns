5a5810ae18b580cff937bdd25232dad994072095
Vitaliy Semenov <synthex@bk.ru>
2024-01-18 11:44:42 +0600

    chore: init visitor module

---
+++ app-modules/visitor/routes/visitor-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Visitor\Http\Controllers\VisitorController;
+
+// Route::get('/visitors', [VisitorController::class, 'index'])->name('visitors.index');
+// Route::get('/visitors/create', [VisitorController::class, 'create'])->name('visitors.create');
+// Route::post('/visitors', [VisitorController::class, 'store'])->name('visitors.store');
+// Route::get('/visitors/{visitor}', [VisitorController::class, 'show'])->name('visitors.show');
+// Route::get('/visitors/{visitor}/edit', [VisitorController::class, 'edit'])->name('visitors.edit');
+// Route::put('/visitors/{visitor}', [VisitorController::class, 'update'])->name('visitors.update');
+// Route::delete('/visitors/{visitor}', [VisitorController::class, 'destroy'])->name('visitors.destroy');
---
+++ app-modules/visitor/tests/VisitorServiceProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\Visitor\Tests;
+
+class VisitorServiceProviderTest extends TestCase
+{
+	// TODO
+}
