6af565b60e2ff8f3ec3db97652bab064916f9679
Vitaliy Semenov <synthex@bk.ru>
2024-01-19 14:07:58 +0600

    chore: init CoR module

---
+++ app-modules/chain_of_responsibilities/routes/chain_of_responsibilities-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\ChainOfResponsibilities\Http\Controllers\ChainOfResponsibilitiesController;
+
+// Route::get('/chain_of_responsibilities', [ChainOfResponsibilitiesController::class, 'index'])->name('chain_of_responsibilities.index');
+// Route::get('/chain_of_responsibilities/create', [ChainOfResponsibilitiesController::class, 'create'])->name('chain_of_responsibilities.create');
+// Route::post('/chain_of_responsibilities', [ChainOfResponsibilitiesController::class, 'store'])->name('chain_of_responsibilities.store');
+// Route::get('/chain_of_responsibilities/{chain_of_responsibility}', [ChainOfResponsibilitiesController::class, 'show'])->name('chain_of_responsibilities.show');
+// Route::get('/chain_of_responsibilities/{chain_of_responsibility}/edit', [ChainOfResponsibilitiesController::class, 'edit'])->name('chain_of_responsibilities.edit');
+// Route::put('/chain_of_responsibilities/{chain_of_responsibility}', [ChainOfResponsibilitiesController::class, 'update'])->name('chain_of_responsibilities.update');
+// Route::delete('/chain_of_responsibilities/{chain_of_responsibility}', [ChainOfResponsibilitiesController::class, 'destroy'])->name('chain_of_responsibilities.destroy');
