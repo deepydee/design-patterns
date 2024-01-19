fe63c899f4bef83c8db741116fe650dad5c8ed12
Vitaliy Semenov <synthex@bk.ru>
2024-01-19 16:10:10 +0600

    feat: implement real world chain of responsibilities

--- app-modules/chain_of_responsibilities/routes/chain_of_responsibilities-routes.php
+++ app-modules/chain_of_responsibilities/routes/chain_of_responsibilities-routes.php
@@ -1,5 +1,7 @@
 <?php
 
 use Modules\ChainOfResponsibilities\Http\Controllers\ChainOfResponsibilitiesConceptualController;
+use Modules\ChainOfResponsibilities\Http\Controllers\ChainOfResponsibilitiesRealWorldController;
 
 Route::get('/cor/conceptual', ChainOfResponsibilitiesConceptualController::class);
+Route::get('/cor/real-world', ChainOfResponsibilitiesRealWorldController::class);
---
+++ app-modules/chain_of_responsibilities/src/Http/Controllers/ChainOfResponsibilitiesRealWorldController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\ChainOfResponsibilities\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Modules\ChainOfResponsibilities\RealWorld\RoleCheckMiddleware;
+use Modules\ChainOfResponsibilities\RealWorld\Server;
+use Modules\ChainOfResponsibilities\RealWorld\ThrottlingMiddleware;
+use Modules\ChainOfResponsibilities\RealWorld\UserExistsMiddleware;
+
+/**
+ * Клиентский код.
+ */
+class ChainOfResponsibilitiesRealWorldController extends Controller
+{
+    public function __invoke(Server $server)
+    {
+        $server->register('admin@example.com', 'admin_pass');
+        $server->register('user@example.com', 'user_pass');
+
+        // Все middleware соединены в цепочки. Клиент может построить различные
+        // конфигурации цепочек в зависимости от своих потребностей.
+        $middleware = new ThrottlingMiddleware(2);
+        $middleware
+            ->linkWith(new UserExistsMiddleware($server))
+            ->linkWith(new RoleCheckMiddleware());
+
+        // Сервер получает цепочку из клиентского кода.
+        $server->setMiddleware($middleware);
+
+        // ...
+
+        do {
+            echo "<br>Enter your email:<br>";
+            $email = 'user@example.com';
+
+            echo "Enter your password:<br>";
+            $password = 'user_pass';
+
+            $success = $server->logIn($email, $password);
+        } while (! $success);
+    }
+}
