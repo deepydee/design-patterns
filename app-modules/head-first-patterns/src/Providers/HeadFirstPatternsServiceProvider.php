e8429ec4d9ea485844cefc55b15aa8667b8d4771
Vitaliy Semenov <synthex@bk.ru>
2024-01-21 15:03:55 +0600

    chore: init head-first

---
+++ app-modules/head-first-patterns/src/Providers/HeadFirstPatternsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\HeadFirstPatterns\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HeadFirstPatternsServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
---
+++ app-modules/head-first-patterns/tests/HeadFirstPatternsServiceProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\HeadFirstPatterns\Tests;
+
+class HeadFirstPatternsServiceProviderTest extends TestCase
+{
+	// TODO
+}
